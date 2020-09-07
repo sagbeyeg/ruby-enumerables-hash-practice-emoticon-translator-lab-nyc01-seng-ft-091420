1b5ac2c05a6e8531026fe36bd6b7245e7a3ad890
Sisanwunmi Agbeyegbe <agbeyegbesisan@gmail.com>
2020-09-07 16:01:51 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -21,20 +21,13 @@ def get_japanese_emoticon(file_path, emoticon
     end
   end
   return "Sorry, that emoticon was not found"
-=======
-  # code goes here
->>>>>>> dbf6ce7223c04330e8e62bacb446b59bcc427711
 end
 
 def get_english_meaning(file_path, emoticon)
   load_library(file_path).each do |key, value|
-<<<<<<< HEAD
     if value[:japanese] == emoticon
       return key
     end
   end
   return "Sorry, that emoticon was not found"
-=======
-    binding.pry
->>>>>>> dbf6ce7223c04330e8e62bacb446b59bcc427711
 end
